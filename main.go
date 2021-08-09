c32edcb97a5a4b14006fa98afee9ac244a1ce994
valiangspe <valian@gspe.co.id>
2021-08-09 23:24:01 +0700

    env add mysql database

--- main.go
+++ main.go
@@ -8,12 +8,30 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Setup database
+	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":3306)/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Opening DB error", err)
+		fmt.Println(db)
+
+		return
 	}
 
 	r := mux.NewRouter()
--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@
 1. Add `.env` containing
 ```sh
 SERVER_PORT=8002
+DB_HOST=dbhost
+DB_USERNAME=dbusername
+DB_PASSWORD=dbpassword
+DB_NAME=dbhost
 ```
 
 2. Install all dependencies on `admin`, `dist`, and `landing`
